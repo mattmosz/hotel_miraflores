20becc33fc0e40aa4cb29ea66600a32c6380bc6d
mattmosz <matias.mosquera619@gmail.com>
2025-03-18 14:53:30 -0500

    feat: se agrega funcionalidad de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../servicio/usuarios.service';
 
 @Component({
   selector: 'app-login',
@@ -6,17 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
   standalone: false
 })
+
+
 export class LoginPage implements OnInit {
 
-  password='';
-  email='';
-
-  constructor() { }
+  correo_usuario: string = '';
+  clave_usuario: string = '';
+  constructor(private usuariosService: UsuariosService) { }
 
   ngOnInit() {
   }
 
-  login(){
+  login() {
+    if (this.correo_usuario && this.clave_usuario) {
+      this.usuariosService.login(this.correo_usuario, this.clave_usuario).subscribe(response => {
+        if (response && response.id_usuario) {
+          console.log('¡Inicio de sesión correcto!', response);
+        } else {
+          console.error('¡Inicio de sesión incorrecto!', response);
+        }
+      }, error => {
+        console.error('¡Error al iniciar sesión!', error);
+      });
+    }
   }
 
   register(){
--- src/app/servicio/usuarios.service.ts
+++ src/app/servicio/usuarios.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  private apiUrl = 'http://localhost/WS_HOTEL/controllers/usuarios.controller.php'; 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(correo_usuario: string, clave_usuario: string): Observable<any> {
+    const params = new HttpParams()
+      .set('op', 'login')
+      .set('correo_usuario', correo_usuario)
+      .set('clave_usuario', clave_usuario);
+
+    return this.http.get<any>(this.apiUrl, { params });
+  }
 }
